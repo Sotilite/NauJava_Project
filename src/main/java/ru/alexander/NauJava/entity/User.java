e37934f30321a67b99be95e6cd5bd153a1e55264
Sasha <govoruxin09042004@gmail.com>
2025-04-01 14:23:48 +0500

    Refactoring

--- src/main/java/ru/alexander/NauJava/entity/User.java
+++ src/main/java/ru/alexander/NauJava/entity/User.java
@@ -3,6 +3,7 @@ package ru.alexander.NauJava.entity;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "Users")
@@ -25,6 +26,9 @@ public class User {
 
     @Column
     private LocalDateTime creationDate;
+
+    @OneToMany
+    private List<Application> applications;
 
     public User() {}
 
@@ -82,6 +86,14 @@ public class User {
 
     public LocalDateTime getCreationDate() {
         return creationDate;
+    }
+
+    public void setApplications(List<Application> applications) {
+        this.applications = applications;
+    }
+
+    public List<Application> getApplications() {
+        return applications;
     }
 }
 
